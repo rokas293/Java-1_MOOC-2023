56891fc6fa8e292fd8c56823091174ef18fee3c9
rokas mitka <83891594+rokas293@users.noreply.github.com>
2023-07-18 14:33:30 +0100

    Add files via upload

---
+++ Part02/part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class SquareRootOfSum {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int first = Integer.valueOf(scanner.nextLine());
+        int second = Integer.valueOf(scanner.nextLine());
+        int sum = first + second;
+        if(sum > 0){
+            double sqrt = Math.sqrt(sum);
+            System.out.println(sqrt);
+        }
+    }
+}
