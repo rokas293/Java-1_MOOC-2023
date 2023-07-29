9ff70b43f9f6df55336f1c2bcb8bf963b4fd9e2a
rokas mitka <83891594+rokas293@users.noreply.github.com>
2023-07-30 00:40:30 +0100

    Add files via upload

---
+++ Part04/part04-Part04_05.Whistle/src/main/java/Whistle.java
@@ -0,0 +1,10 @@
+public class Whistle {
+    private String sound;
+    
+    public Whistle(String whistleString){
+        this.sound = whistleString;
+    }
+    public void sound(){
+        System.out.println(this.sound);
+    }
+}
